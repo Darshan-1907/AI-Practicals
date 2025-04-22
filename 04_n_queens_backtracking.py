2d28d8c019a694f39c0cf357e5ef5f139991a6e2
Darshan Patil <110981147+Darshan-1907@users.noreply.github.com>
2025-04-22 23:18:26 +0530

    Update 04_n_queens_backtracking.py

--- 04_n_queens_backtracking.py
+++ 04_n_queens_backtracking.py
@@ -1,58 +1,47 @@
-def solveNQueens(n: int):
-    res = []
-    board = [['.' for _ in range(n)] for _ in range(n)]
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+    print()
 
-    def isSafe(row, col):
-        # check in column
-        for i in range(row):    
-            if (board[i][col] == 'Q'):
-                return False
-            
-        # check in positive diagonal
-        i = row
-        j = col
-        while (i >= 0 and j < n):
-            if (board[i][j] == 'Q'):
-                return False
-            i -= 1
-            j += 1
-
-        # check in negative diagonal
-        i = row
-        j = col
-        while (i >= 0 and j >= 0):
-            if (board[i][j] == 'Q'):
-                return False
-            i -= 1
-            j -= 1
-
-        return True
-
-    def backtrack(row):
-        if (row == n):
-            res.append([''.join(row) for row in board])
+def is_safe(board, row, col, n):
+    # Check column
+    for i in range(row):
+        if board[i][col] == 'Q':
             return False
-        for col in range(n):
-            if (isSafe(row, col)):
-                board[row][col] = 'Q'
-                possible = backtrack(row+1)
-                if (possible):
-                    return True
-                board[row][col] = '.'
-        return False
-    
-    backtrack(0)
-    return res
 
-def printSolutions(boards):
-    for board in enumerate(boards):
-        print(f"Solution {board[0]+1}")
-        for row in board[1]:
-            for col in row:
-                print(col, end=' ')
-            print()
-        print()
+    # Check upper left diagonal
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 'Q':
+            return False
+        i -= 1
+        j -= 1
 
-if __name__ == "__main__":
-    boards = solveNQueens(4)
-    printSolutions(boards)
+    # Check upper right diagonal
+    i, j = row, col
+    while i >= 0 and j < n:
+        if board[i][j] == 'Q':
+            return False
+        i -= 1
+        j += 1
+
+    return True
+
+def solve_n_queens(board, row, n):
+    if row == n:
+        print_board(board)
+        return
+
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row][col] = 'Q'  # Place queen
+            solve_n_queens(board, row + 1, n)
+            board[row][col] = '.'  # Backtrack
+
+def n_queens(n):
+    board = [['.' for _ in range(n)] for _ in range(n)]
+    solve_n_queens(board, 0, n)
+
+# Run the algorithm for N = 4
+n = 4
+n_queens(n)
